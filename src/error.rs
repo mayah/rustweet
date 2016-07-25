e54a5431cae852315566903675ffea6b9ab56d77
mayah <mayah@mayah.jp>
2016-07-25 22:38:03 +0900

    Satisfy std::error::Error trait for Error
    Error should satisfy std::error::Error trait.
    See https://doc.rust-lang.org/std/error/

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,40 @@
 extern crate oauth_client as oauth;
 extern crate rustc_serialize;
 
-use std::str;
+use std::{error, fmt, str};
 use rustc_serialize::json;
 
+#[derive(Debug)]
 pub enum Error {
     InvalidError(&'static str),  // Something invalid.
     OAuthError(oauth::Error),
     Utf8Error(str::Utf8Error),
     ParserError(json::ParserError),
     DecorderError(json::DecoderError),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::InvalidError(ref err) => write!(f, "Invalid json error: {}", err),
+            Error::OAuthError(ref err) => write!(f, "OAuth error: {}", err),
+            Error::Utf8Error(ref err) => write!(f, "UTF8 conversion error: {}", err),
+            Error::ParserError(ref err) => write!(f, "JSON parse error: {}", err),
+            Error::DecorderError(ref err) => write!(f, "JSON decoding error: {}", err),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::InvalidError(ref err) => err,
+            Error::OAuthError(ref err) => err.description(),
+            Error::Utf8Error(ref err) => err.description(),
+            Error::ParserError(ref err) => err.description(),
+            Error::DecorderError(ref err) => err.description(),
+        }
+    }
 }
 
 impl From<oauth::Error> for Error {
